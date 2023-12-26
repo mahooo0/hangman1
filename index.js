df59af1be14184dee6647204f156cd785989b0cc
mahooo0 <sevinmuhammed06@gmail.com>
2023-12-27 00:59:19 +0400

    end

---
+++ index.js
@@ -0,0 +1,4 @@
+const Main_obj = {
+    band_names:["blondie","genesis", "inxs" ,"journey","madonna","metalica","poison","queen","toto" ,"uto"]
+};
+document.addEventListener("keypress",(event)=>console.log(event.key))
